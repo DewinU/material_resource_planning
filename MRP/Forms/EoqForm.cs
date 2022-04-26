96bbbe56889762e1c9475339651ea2e0bd9fe89b
Dewin Umana <umana.dewin@gmail.com>
2022-04-25 21:05:58 -0600

    Sistema de control de inventario (#6)
    Co-authored-by: Randall Hodgson <randallhodgson3@gmail.com>

--- MRP/Forms/EoqForm.cs
+++ MRP/Forms/EoqForm.cs
@@ -1,22 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MaterialSkin;
+using System;
 using System.Windows.Forms;
 
 namespace MRP.Forms
 {
     public partial class EoqForm : UserControl
     {
+        public Panel panelCont;
         public EoqForm()
         {
             InitializeComponent();
-
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Grey600, Primary.Grey900, Primary.Grey500, Accent.Indigo100, TextShade.WHITE);
             Console.WriteLine(F(0.99));
+        }
+
+        public EoqForm(Panel panel)
+        {
+            InitializeComponent();
+            panelCont = panel;
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Grey600, Primary.Grey900, Primary.Grey500, Accent.Indigo100, TextShade.WHITE);
+            solRichTextBox.Enabled = false;
+            txtCostoUnitario.Text = "10";
+            txtDemanda.Text = "200";
+            txtMantenimiento.Text = "2";
+            txtNivelServicio.Text = "95";
+            txtPedido.Text = "20";
+            txtStdev.Text =  "10";
+            numPlazo.Value = 4;
+
         }
 
         //metodo para sacar z con probabilidad
@@ -25,6 +40,67 @@ namespace MRP.Forms
             var curve = new MathNet.Numerics.Distributions.Normal();
             var z_value = curve.InverseCumulativeDistribution(x);
             return (z_value);
+        }
+
+        private void EoqForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModelP_Click(object sender, EventArgs e)
+        {
+            var prov = new InventoryPForm(panelCont);
+            panelCont.Controls.Clear();
+            prov.Dock = DockStyle.Fill;
+            panelCont.Controls.Add(prov);
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtCostoUnitario.Text) || string.IsNullOrEmpty(txtDemanda.Text) || string.IsNullOrEmpty(txtMantenimiento.Text) || string.IsNullOrEmpty(txtNivelServicio.Text)
+                    || string.IsNullOrEmpty(txtPedido.Text) || string.IsNullOrEmpty(txtStdev.Text) || (numPlazo.Value < 0)) {
+                MessageBox.Show("Todos los campos son obligatorios", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if(txtMantenimiento.Text == "0")
+            {
+                MessageBox.Show("El costo de mantenimiento no debe ser de 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return ;
+            }
+            int eoq = calcModelQ(int.Parse(txtDemanda.Text), float.Parse(txtPedido.Text), int.Parse(txtDiasLaborales.Text), float.Parse(txtMantenimiento.Text));
+            double zScore = F(double.Parse(txtNivelServicio.Text)/100);
+            int securityStock = calcSecurityStock(int.Parse(txtStdev.Text), zScore);
+            int rop = calcRop(int.Parse(txtDemanda.Text), int.Parse(numPlazo.Value.ToString()), securityStock);
+            double costoTotal = calcCosto(int.Parse(txtDemanda.Text), double.Parse(txtPedido.Text), double.Parse(txtMantenimiento.Text), eoq, double.Parse(txtCostoUnitario.Text));
+            solRichTextBox.Text = $"Si se establece un sistema de administración de inventario de revisión continua se deben hacer pedidos de {eoq.ToString("N0")} unidades. Además se debe estar revisando continuamente las existencias del producto. " +
+                $"Vigilando que cuando las existencias sean menores o iguales a {rop.ToString("N0")} unidades se realice un nuevo pedido. El inventario de seguridad con este sistema es de {securityStock.ToString("N0")} unidades y tiene un costo de {costoTotal.ToString("C3")} unidades monetarias.";
+        }
+
+        private int calcModelQ(int demanda, float costoPedido, int diasLaborales, float costoMantenimiento )
+        {
+            return (int) Math.Ceiling(Math.Sqrt((2 * demanda * diasLaborales * costoPedido) / costoMantenimiento));
+        }
+
+        private int calcRop(int demanda, int plazo, int securityStock)
+        {
+            return ((plazo * demanda) + securityStock);
+        }
+
+        private int calcSecurityStock(int stdev, double zScore)
+        {
+            var stdevPeriodo = stdev * Math.Sqrt((double)numPlazo.Value);
+            Console.WriteLine(stdev);
+            Console.WriteLine(numPlazo.Value); ;
+            return (int) Math.Ceiling(stdevPeriodo * zScore);
+        } 
+
+        private double calcCosto(int demanda, double pedido, double mantenimiento, int eoq, double costo)
+        {
+            double costoOrdenamiento = (pedido * demanda * int.Parse(txtDiasLaborales.Text)) / eoq;
+            double costoMantenimiento = (mantenimiento * eoq) / 2;
+            double costoProd = demanda * int.Parse(txtDiasLaborales.Text) * costo;
+            return (costoOrdenamiento + costoMantenimiento + costoProd);
         }
     }
 }
---
+++ MRP/Forms/InventoryPForm.cs
@@ -0,0 +1,118 @@
+﻿using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MRP.Forms
+{
+    public partial class InventoryPForm : UserControl
+    {
+        public Panel panelCont;
+        public InventoryPForm()
+        {
+            InitializeComponent();
+        }
+
+        public InventoryPForm(Panel panel)
+        {
+            InitializeComponent();
+            panelCont = panel;
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Grey600, Primary.Grey900, Primary.Grey500, Accent.Indigo100, TextShade.WHITE);
+            txtDemanda.Text = "10";
+            numPlazo.Value = 30;
+            numRevision.Value = 14;
+            txtStdev.Text = "3";
+            txtInventario.Text = "150";
+            txtNivelServicio.Text = "98";
+        }
+        static double F(double x)
+        {
+            var curve = new MathNet.Numerics.Distributions.Normal();
+            var z_value = curve.InverseCumulativeDistribution(x);
+            return (z_value);
+        }
+
+        private void InventoryPForm_Load(object sender, EventArgs e)
+        {
+            txtDemanda.Text = "10";
+            numPlazo.Value = 30;
+            numRevision.Value = 14;
+            txtStdev.Text = "3";
+            txtInventario.Text = "150";
+
+        }
+
+        private void btnModelQ_Click(object sender, EventArgs e)
+        {
+            var prov = new EoqForm(panelCont);
+            panelCont.Controls.Clear();
+            prov.Dock = DockStyle.Fill;
+            panelCont.Controls.Add(prov);
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCostoUnitario.Text) || string.IsNullOrEmpty(txtDemanda.Text) || string.IsNullOrEmpty(txtMantenimiento.Text) || string.IsNullOrEmpty(txtNivelServicio.Text)
+                    || string.IsNullOrEmpty(txtPedido.Text) || string.IsNullOrEmpty(txtStdev.Text) || (numPlazo.Value < 0))
+            {
+                MessageBox.Show("Todos los campos son obligatorios", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (txtMantenimiento.Text == "0")
+            {
+                MessageBox.Show("El costo de mantenimiento no debe ser de 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double zScore = F(double.Parse(txtNivelServicio.Text) / 100);
+            int ss = calcSecurityStock(int.Parse(txtStdev.Text), zScore);
+            int cantidad = calcModelP(int.Parse(txtDemanda.Text), int.Parse(numPlazo.Value.ToString()), int.Parse(numRevision.Value.ToString()), int.Parse(txtStdev.Text), int.Parse(txtInventario.Text), ss);
+
+            //string costo = calcCosto(int.Parse(txtDemanda.Text), int.Parse(txtPedido.Text), int.Parse(txtMantenimiento.Text), int.Parse(cantidad), double.Parse(txtCostoUnitario.Text)).ToString("C3");
+            solRichTextBox.Text = $"Si se establece un sistema de administración de inventario de periodos fijos se deben hacer pedidos de {cantidad.ToString("N0")} unidades. Además se debe de hacer pedidos cada {numRevision.Value} periodos." +
+                $" El inventario de seguridad con este sistema es de {ss.ToString("N0")} unidades y tiene un costo de {{costo}} unidades monetarias.";
+
+
+        }
+
+        private int calcModelP(int demanda, int revisiones, int entrega, int stdev, int inventario, int ss)
+        {
+            int demandaVulnerable = demanda * (revisiones + entrega);
+            Console.WriteLine(demandaVulnerable);
+            return (int)demandaVulnerable + ss - inventario;
+        }
+
+        private int calcSecurityStock(int stdev, double zScore)
+        {
+            var stdevPeriodo = stdev * Math.Sqrt((int)numPlazo.Value + (int)numRevision.Value);
+            Console.WriteLine(zScore);
+            return (int)(stdevPeriodo * zScore);
+        }
+
+        private double calcCosto(int demanda, double pedido, double mantenimiento, int eoq, double costo)
+        {
+            double costoOrdenamiento = (pedido * demanda * int.Parse(txtDiasLaborales.Text)) / eoq;
+            double costoMantenimiento = (mantenimiento * eoq) / 2;
+            double costoProd = demanda * int.Parse(txtDiasLaborales.Text) * costo;
+            return (costoOrdenamiento + costoMantenimiento + costoProd);
+        }
+
+        private void btnModelQ_Click_1(object sender, EventArgs e)
+        {
+
+            var prov = new EoqForm(panelCont);
+            panelCont.Controls.Clear();
+            prov.Dock = DockStyle.Fill;
+            panelCont.Controls.Add(prov);
+        }
+    }
+}
