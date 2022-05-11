3ecf394e1cb783044c3f536202dde1ecf6473829
armando17 <yiin.yeh.hsing12@gmail.com>
2022-05-10 21:01:34 -0600

    Termine nais

---
+++ MRP/Calculos/Estrategias_Plan_Agregado.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MRP.Calculos
+{
+    public class Estrategias_Plan_Agregado
+    {
+        public static int[] make_ss(DataGridView ss, int periodos, float tasa_ss, int[] demandas, int inv_ini)
+        {
+            //hacer tabla ss
+            ss.Columns.Clear();
+            ss.Rows.Clear();
+            int stock;
+            int req_prod;
+            int[] lista_req_prod = new int[periodos];
+
+            //columnas ss
+            for (int i = 0; i < periodos; i++)
+            {
+
+                ss.Columns.Add("periodo_" + (i + 1).ToString(), "Periodo " + (i + 1).ToString());
+            }
+            //filas ss
+            ss.Rows.Add();
+            ss.Rows.Add();
+            ss.Rows.Add();
+            ss.Rows.Add();
+            ss.Rows[0].HeaderCell.Value = "Demanda";
+            ss.Rows[1].HeaderCell.Value = "SS";
+            ss.Rows[2].HeaderCell.Value = "Inv Inicial";
+            ss.Rows[3].HeaderCell.Value = "Req de Prod";
+            ss.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+
+
+            for (int i = 0; i < periodos; i++)
+            {
+                stock = Convert.ToInt32(tasa_ss * demandas[i]);
+                req_prod = demandas[i] + stock - inv_ini;
+                ss.Rows[0].Cells[i].Value = demandas[i];
+                ss.Rows[1].Cells[i].Value = stock;
+                ss.Rows[2].Cells[i].Value = inv_ini;
+                ss.Rows[3].Cells[i].Value = req_prod;
+                inv_ini = stock;
+                lista_req_prod[i] = req_prod;
+            }
+            return lista_req_prod;
+        }
+        public static void persecucion(DataGridView ss, DataGridView main, Label lbl_total, float c_contratacion, float c_despido, float c_hrs_n, float hrs_diarias, float tasa_ss, int inv_ini, int periodos, int[] demandas, int[] dias, float t_elab)
+        {
+            main.Rows.Clear();
+            main.Columns.Clear();
+            float c_total_estrategia = 0;
+            int[] req_list  = new int[periodos];
+            req_list = make_ss(ss, periodos, tasa_ss, demandas, inv_ini);
+
+            //hacer tabla main
+            for (int i = 0; i < periodos; i++)
+            {
+                main.Columns.Add("periodo_" + (i + 1).ToString(), "Periodo " + (i + 1).ToString());
+            }
+
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows[0].HeaderCell.Value = "Req de Prod";
+            main.Rows[1].HeaderCell.Value = "Hrs de Prod Req";
+            main.Rows[2].HeaderCell.Value = "Dias Habiles x Mes";
+            main.Rows[3].HeaderCell.Value = "Hrs Disponibles";
+            main.Rows[4].HeaderCell.Value = "Trab Req";
+            main.Rows[5].HeaderCell.Value = "Trab Contratados";
+            main.Rows[6].HeaderCell.Value = "C Contratar";
+            main.Rows[7].HeaderCell.Value = "Trab Despedidos";
+            main.Rows[8].HeaderCell.Value = "C Despedir";
+            main.Rows[9].HeaderCell.Value = "Costo T Normal";
+            main.Rows[10].HeaderCell.Value = "Costo Total";
+
+            main.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+
+            for (int i = 0; i < periodos; i++)
+            {
+                main.Rows[0].Cells[i].Value = req_list[i];
+                main.Rows[1].Cells[i].Value = (float)req_list[i] * t_elab;
+                main.Rows[2].Cells[i].Value = dias[i];
+                main.Rows[3].Cells[i].Value = dias[i] * hrs_diarias;
+                main.Rows[4].Cells[i].Value = Math.Round((float)main.Rows[1].Cells[i].Value / (float)main.Rows[3].Cells[i].Value);
+
+                
+                if(i == 0)
+                {
+                    //falta validar f lab inical
+                    main.Rows[5].Cells[i].Value = 0;
+                    main.Rows[7].Cells[i].Value = 0;
+
+                    main.Rows[6].Cells[i].Value = 0;
+                    main.Rows[8].Cells[i].Value = 0;
+                }
+                else
+                {
+                    if (int.Parse(main.Rows[4].Cells[i - 1].Value.ToString()) - int.Parse(main.Rows[4].Cells[i].Value.ToString()) > 0)
+                    {
+                        main.Rows[5].Cells[i].Value = 0;
+                    }
+                    else
+                    {
+                        main.Rows[5].Cells[i].Value = Math.Abs(int.Parse(main.Rows[4].Cells[i - 1].Value.ToString()) - int.Parse(main.Rows[4].Cells[i].Value.ToString()));
+                    }
+
+                    main.Rows[6].Cells[i].Value = float.Parse(main.Rows[5].Cells[i].Value.ToString()) * c_contratacion;
+
+                    if (int.Parse(main.Rows[4].Cells[i - 1].Value.ToString()) - int.Parse(main.Rows[4].Cells[i].Value.ToString()) >= 1)
+                    {
+                        main.Rows[7].Cells[i].Value = Math.Abs(int.Parse(main.Rows[4].Cells[i - 1].Value.ToString()) - int.Parse(main.Rows[4].Cells[i].Value.ToString()));
+                    }
+                    else
+                    {
+                        main.Rows[7].Cells[i].Value = 0;
+                    }
+                    main.Rows[8].Cells[i].Value = float.Parse(main.Rows[7].Cells[i].Value.ToString()) * c_despido;
+                }
+                main.Rows[9].Cells[i].Value = float.Parse(main.Rows[1].Cells[i].Value.ToString()) * c_hrs_n;
+                main.Rows[10].Cells[i].Value = float.Parse(main.Rows[6].Cells[i].Value.ToString()) + float.Parse(main.Rows[8].Cells[i].Value.ToString()) + float.Parse(main.Rows[9].Cells[i].Value.ToString());
+                c_total_estrategia += float.Parse(main.Rows[10].Cells[i].Value.ToString());
+            }
+
+            lbl_total.Text ="Costo Total Estrategia: " + c_total_estrategia.ToString();
+
+        }
+        public static void fuerza_nivelada(DataGridView ss, DataGridView main, Label lbl_total, float c_hrs_n, float hrs_diarias, float tasa_ss, int inv_ini, int periodos, int[] demandas, int[] dias, float t_elab, float c_faltante, float h)
+        {
+            main.Rows.Clear();
+            main.Columns.Clear();
+            float c_total_estrategia = 0;
+            int[] req_list = new int[periodos];
+            req_list = make_ss(ss, periodos, tasa_ss, demandas, inv_ini);
+
+
+            //hacer main
+            //columnas
+            for (int i = 0; i < periodos; i++)
+            {
+                main.Columns.Add("periodo_" + (i + 1).ToString(), "Periodo " + (i + 1).ToString());
+            }
+            //filas
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows[0].HeaderCell.Value = "Inv Ini";
+            main.Rows[1].HeaderCell.Value = "Dias x Mes";
+            main.Rows[2].HeaderCell.Value = "Hrs Disponibles";
+            main.Rows[3].HeaderCell.Value = "Prod Real";
+            main.Rows[4].HeaderCell.Value = "Pronostico";
+            main.Rows[5].HeaderCell.Value = "Inv Final";
+            main.Rows[6].HeaderCell.Value = "C Faltante";
+            main.Rows[7].HeaderCell.Value = "SS";
+            main.Rows[8].HeaderCell.Value = "Exceso Unds";
+            main.Rows[9].HeaderCell.Value = "H";
+            main.Rows[10].HeaderCell.Value = "C T Normal";
+            main.Rows[11].HeaderCell.Value = "C Total";
+            main.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+            
+            
+            //calcular trabajadores promedio
+
+            int No_Trab_Prom = (req_list.Sum() * (int)t_elab) / (dias.Sum() * (int)hrs_diarias);
+            Console.WriteLine(No_Trab_Prom);
+            
+            for (int i = 0; i < periodos; i++)
+            {
+                main.Rows[0].Cells[i].Value = inv_ini;
+                main.Rows[1].Cells[i].Value = dias[i];
+                main.Rows[2].Cells[i].Value = dias[i] * 8 * No_Trab_Prom;
+                main.Rows[3].Cells[i].Value = float.Parse(main.Rows[2].Cells[i].Value.ToString()) / t_elab;
+                main.Rows[4].Cells[i].Value = demandas[i];
+                main.Rows[5].Cells[i].Value = inv_ini + int.Parse(main.Rows[3].Cells[i].Value.ToString()) - int.Parse(main.Rows[4].Cells[i].Value.ToString());
+
+                if (int.Parse(main.Rows[5].Cells[i].Value.ToString()) < 0)
+                {
+                    main.Rows[6].Cells[i].Value = Math.Abs(float.Parse(main.Rows[5].Cells[i].Value.ToString()) * c_faltante);
+                }
+                else
+                {
+                    main.Rows[6].Cells[i].Value = 0;
+                }
+                main.Rows[7].Cells[i].Value = int.Parse(ss.Rows[1].Cells[i].Value.ToString());
+                if (int.Parse(main.Rows[5].Cells[i].Value.ToString()) - int.Parse(main.Rows[7].Cells[i].Value.ToString()) > 0)
+                {
+                    main.Rows[8].Cells[i].Value = int.Parse(main.Rows[5].Cells[i].Value.ToString()) - int.Parse(main.Rows[7].Cells[i].Value.ToString());
+                }
+                else
+                {
+                    main.Rows[8].Cells[i].Value = 0;
+                }
+                main.Rows[9].Cells[i].Value = Math.Round(int.Parse(main.Rows[8].Cells[i].Value.ToString()) * h);
+                main.Rows[10].Cells[i].Value = float.Parse(main.Rows[2].Cells[i].Value.ToString()) * c_hrs_n;
+                main.Rows[11].Cells[i].Value = float.Parse(main.Rows[6].Cells[i].Value.ToString()) + float.Parse(main.Rows[9].Cells[i].Value.ToString()) + float.Parse(main.Rows[10].Cells[i].Value.ToString());
+                
+                c_total_estrategia += float.Parse(main.Rows[11].Cells[i].Value.ToString());
+
+                inv_ini = int.Parse(main.Rows[5].Cells[i].Value.ToString());
+            }
+            lbl_total.Text = "Costo Total Estrategia: " + c_total_estrategia.ToString();
+        }
+        public static void outsourcing(DataGridView ss, DataGridView main, Label lbl_total, float c_hrs_n, float hrs_diarias, float tasa_ss, int inv_ini, int periodos, int[] demandas, int[] dias, float t_elab,float c_outsourcing)
+        {
+            main.Rows.Clear();
+            main.Columns.Clear();
+            float c_total_estrategia = 0;
+            int[] req_list = new int[periodos];
+            req_list = make_ss(ss, periodos, tasa_ss, demandas, inv_ini);
+
+            //hacer tabla main
+            for (int i = 0; i < periodos; i++)
+            {
+                main.Columns.Add("periodo_" + (i + 1).ToString(), "Periodo " + (i + 1).ToString());
+            }
+
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows.Add();
+            main.Rows[0].HeaderCell.Value = "Req de Prod";
+            main.Rows[1].HeaderCell.Value = "Dias Habiles";
+            main.Rows[2].HeaderCell.Value = "Hrs Disponibles";
+            main.Rows[3].HeaderCell.Value = "Prod Real";
+            main.Rows[4].HeaderCell.Value = "Und SubContratadas";
+            main.Rows[5].HeaderCell.Value = "C SubContratar";
+            main.Rows[6].HeaderCell.Value = "C Hrs N";
+            main.Rows[7].HeaderCell.Value = "C Total";
+
+            main.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+            //calcular trabajadores promedio
+            int No_Trab_Prom = (req_list.Sum() * (int)t_elab) / (dias.Sum() * (int)hrs_diarias);
+            Console.WriteLine(No_Trab_Prom);
+            for (int i = 0; i < periodos; i++)
+            {
+                main.Rows[0].Cells[i].Value = req_list[i];
+                main.Rows[1].Cells[i].Value = dias[i];
+                main.Rows[2].Cells[i].Value = dias[i] * hrs_diarias * No_Trab_Prom;
+                main.Rows[3].Cells[i].Value = float.Parse(main.Rows[2].Cells[i].Value.ToString()) / t_elab;
+                main.Rows[4].Cells[i].Value = float.Parse(main.Rows[0].Cells[i].Value.ToString()) - float.Parse(main.Rows[3].Cells[i].Value.ToString());
+                main.Rows[5].Cells[i].Value = float.Parse(main.Rows[4].Cells[i].Value.ToString()) * c_outsourcing;
+                main.Rows[6].Cells[i].Value = float.Parse(main.Rows[2].Cells[i].Value.ToString()) * c_hrs_n;
+                main.Rows[7].Cells[i].Value = float.Parse(main.Rows[5].Cells[i].Value.ToString()) + float.Parse(main.Rows[6].Cells[i].Value.ToString());
+                c_total_estrategia += float.Parse(main.Rows[7].Cells[i].Value.ToString());
+            }
+            lbl_total.Text = "Costo Total Estrategia: " + c_total_estrategia.ToString();
+        }
+    }
+}
--- MRP/Forms/PlanForm.cs
+++ MRP/Forms/PlanForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MRP.Calculos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,148 @@ namespace MRP.Forms
         public PlanForm()
         {
             InitializeComponent();
+        }
+        public int periodos = 0;
+        public float costo;
+        public float H;
+        public float c_faltante;
+        public float c_outsourcing;
+        public float c_contratar;
+        public float c_despido;
+        public float c_hrs_n;
+        public float c_hrs_e;
+        public int f_lab;
+        public float tasa_ss;
+        public int inv_ini;
+        public int hrs_diarias;
+        public float t_elab;
+
+        public int[] demandas, dias;
+
+
+        private void BTN_Aceptar_Click(object sender, EventArgs e)
+        {
+            if ( string.IsNullOrEmpty(TXT_Periodos.Text) || string.IsNullOrWhiteSpace(TXT_Periodos.Text))
+            {
+                MessageBox.Show("Campo Vacio, intente de nuevo", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            else
+            {
+                DGV_Demanda.Rows.Clear();
+                DGV_Demanda.Columns.Clear();
+                periodos = int.Parse(TXT_Periodos.Text);
+                //DGV_Demanda.ColumnCount = periodos;
+
+
+                for (int i = 0; i < periodos; i++)
+                {
+                    /*
+                    DGV_Demanda.Columns[i].Name = "Periodo " + (i + 1).ToString();
+                    Console.WriteLine(DGV_Demanda.Columns[i].Name);
+                    */
+
+                    DGV_Demanda.Columns.Add("periodo_" + (i + 1).ToString(), "Periodo " + (i + 1).ToString());
+                    Console.WriteLine(DGV_Demanda.Columns[i].Name);
+
+                }
+                DGV_Demanda.Rows.Add();
+                DGV_Demanda.Rows.Add();
+                DGV_Demanda.Rows[0].HeaderCell.Value = "Demanda";
+                DGV_Demanda.Rows[1].HeaderCell.Value = "Dias Laborados";
+                DGV_Demanda.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+                demandas = new int[periodos];
+                dias = new int[periodos];
+            }
+        }
+
+        private void CBO_Estrategias_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(CBO_Estrategias.SelectedIndex);
+
+        }
+
+        private void BTN_Limpiar_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Desea Limpiar todos los cuadros de texto?", "Advertencia",MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                TXT_Contratar.Text = "";
+                TXT_Costo.Text = "";
+                TXT_C_Hrs_E.Text = "";
+                TXT_C_Hr_N.Text = "";
+                TXT_Despedir.Text = "";
+                TXT_Faltante.Text = "";
+                TXT_F_Lab.Text = "";
+                TXT_H.Text = "";
+                TXT_Hrs_Lab.Text = "";
+                TXT_Inv_Ini.Text = "";
+                TXT_Outs.Text = "";
+                TXT_tasa_SS.Text = "";
+                TXT_T_Elab.Text = "";
+                DGV_Demanda.Columns.Clear();
+                DGV_Demanda.Rows.Clear();
+                DGV_SS.Columns.Clear();
+                DGV_SS.Rows.Clear();
+                DGV_Plan_Agregado.Columns.Clear();
+                DGV_Plan_Agregado.Rows.Clear();
+            }
+        }
+
+        private void BTN_Calcular_Click(object sender, EventArgs e)
+        {
+            /*
+            var result = MessageBox.Show("No posee Fuerza Laboral Estandar? Se asumira la Fuerza laboral calculada en el primer mes como Inical 
+            y para la estrategia de Fuerza Nivelada, Se hara promedio de trabajadores durante los meses. ", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            { 
+                
+            }
+            else
+            {
+                return;
+            }
+            */
+
+            c_contratar = float.Parse(TXT_Contratar.Text);
+            costo = float.Parse(TXT_Costo.Text);
+            c_hrs_n = float.Parse(TXT_C_Hr_N.Text);
+            c_hrs_e = float.Parse(TXT_C_Hrs_E.Text);
+            c_despido = float.Parse(TXT_Despedir.Text);
+            c_faltante = float.Parse(TXT_Faltante.Text);
+            f_lab = int.Parse(TXT_F_Lab.Text);
+            H = float.Parse(TXT_H.Text);
+            hrs_diarias = int.Parse(TXT_Hrs_Lab.Text);
+            inv_ini = int.Parse(TXT_Inv_Ini.Text);
+            c_outsourcing = float.Parse(TXT_Outs.Text);
+            tasa_ss = float.Parse(TXT_tasa_SS.Text);
+            t_elab = float.Parse(TXT_T_Elab.Text);
+
+            
+            
+
+            for (int i = 0; i < periodos; i++)
+            {
+                demandas[i] = int.Parse(DGV_Demanda.Rows[0].Cells[i].Value.ToString());
+                dias[i] = int.Parse(DGV_Demanda.Rows[1].Cells[i].Value.ToString());
+            }
+
+
+            switch (CBO_Estrategias.SelectedIndex)
+            {
+                case 0:
+                    Estrategias_Plan_Agregado.persecucion(DGV_SS, DGV_Plan_Agregado, LBL_Total, c_contratar, c_despido, c_hrs_n, hrs_diarias, tasa_ss, inv_ini, periodos, demandas, dias, t_elab);
+                    break;
+                case 1:
+                    Estrategias_Plan_Agregado.fuerza_nivelada(DGV_SS, DGV_Plan_Agregado, LBL_Total, c_hrs_n, hrs_diarias, tasa_ss, inv_ini, periodos, demandas, dias, t_elab, c_faltante, H);
+                    break;
+                case 2:
+                    Estrategias_Plan_Agregado.outsourcing(DGV_SS, DGV_Plan_Agregado, LBL_Total, c_hrs_n, hrs_diarias, tasa_ss, inv_ini, periodos, demandas, dias, t_elab, c_outsourcing);
+                    break;
+                default:
+
+                    break;
+            }
         }
     }
 }
